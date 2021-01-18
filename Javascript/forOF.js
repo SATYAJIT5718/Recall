091dd52c385578a50d0186207775b3530819e987
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-18 11:20:42 +0530

    update

--- Javascript/forOF.js
+++ Javascript/forOF.js
@@ -1,6 +1,7 @@
 let num=[1,2,3,4,5]
-for( x of num ){
-    console.log(x);
+    for( x of num ){
+    console.log("hello",":",x);
+    // console.log(x);
 }
 
 // (for of) is always used for invocation purpose in ARRAY.
---
+++ Javascript/forin.js
@@ -0,0 +1,10 @@
+let obj={
+    name:'satya',
+    age:26,
+    location:"banglore",
+    blood_group:"AB+ve"
+}
+
+for(x of obj){
+    console.log(x)
+}
