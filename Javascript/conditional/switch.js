91922e67b5ca1b5f8d9f9461458052a47c00af77
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-18 18:13:23 +0530

    update

---
+++ Javascript/conditional/switch.js
@@ -0,0 +1,9 @@
+let day="moday";
+switch(day){
+    case monday:
+        console.log("Today is monday");
+        break;
+    case tuesday:
+        console.log("today");
+    default("Today is sunday"):
+}
