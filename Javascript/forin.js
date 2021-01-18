dc56f92ba71a7662bf741e2e39fac82d5a8da8cb
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-18 11:28:26 +0530

    update

--- Javascript/forin.js
+++ Javascript/forin.js
@@ -5,6 +5,9 @@ let obj={
     blood_group:"AB+ve"
 }
 
-for(x of obj){
-    console.log(x)
+for(x in obj){
+    console.log(x ,":",obj[x])
+    console.log(x); // this is only showing the property name not property value.
 }
+
+// for in is used for the iteration purpose in OBJECT.
