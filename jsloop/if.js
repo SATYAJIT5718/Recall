97c428d471aee7db1e73267abb09b8b3641d4949
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-18 17:07:46 +0530

    update

---
+++ jsloop/if.js
@@ -0,0 +1,9 @@
+let x=[1,2,3,4,5,6]
+// console.log(x);
+// console.log(x[2]);
+// console.log(x.lenght);
+x.pop();// to remove last element
+x.shift();// it remove 1st element
+x.unshift("s"); // it add new element in the 1st place of the array
+x.push('g'); // it add new element in the end of the array
+console.log(x);
---
+++ jsloop/test.js
@@ -0,0 +1,7 @@
+let hi=['alok',57,{},"cool",9,[]]
+// hi.pop();
+hi.push(65);
+hi.shift();
+hi.unshift(546)
+console.log(hi);
+// console.error('cool');
