49050b53580967744a0b3830a1289b544a4b98da
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-21 11:50:01 +0530

    switch loop of day

--- Javascript/conditional/switch.js
+++ Javascript/conditional/switch.js
@@ -1,14 +1,14 @@
-// let day="tuesday";
-// switch(day){
-//     case "monday":
-//         console.log("Today is ",day);
-//         break;
-//     case "tuesday":
-//         console.log("today is ",day);
-//         break;
-//     default:
-//         console.log("Today is sunday");
-//         break;
-// }
+let day="tuesday";
+switch(day){
+    case "monday":
+        console.log("Today is ",day);
+        break;
+    case "tuesday":
+        console.log("today is ",day);
+        break;
+    default:
+        console.log("Today is sunday");
+        break;
+}
 
-console.log(new Date().getDay());
+// console.log(new Date().getDay());// by this code we can get current day represent as 0-6.
