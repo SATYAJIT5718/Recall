e2212ba5aee7b4b5cd362982ecbd17b96c718552
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-25 22:58:39 +0530

    function recall

---
+++ Javascript/function/one.js
@@ -0,0 +1,14 @@
+function colorchange(){
+    alert ("Click me,then check the button color will be change....");
+    console.log("OMG,it's working");
+    document.getElementById('kill').style.background='red';
+}
+
+function zoom(){
+    // alert("yes,it's working..")
+    document.getElementById('focus').style.fontSize='50px';
+}
+
+function zoomout(){
+    document.getElementById('focus').style.fontSize='large';
+}
